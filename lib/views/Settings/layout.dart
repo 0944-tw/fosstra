752af30f4fd03f3063f264286a23b978f83a7079
zeroninefourfour <zwec181818@proton.me>
2025-09-13 23:09:35 +0800

    Settings Page Layout [NEW/SettingsLayout]

---
+++ lib/views/Settings/layout.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tra/l10n/app_localizations.dart';
+
+class SettingsLayout extends StatefulWidget {
+  final Widget child;
+  final String title;
+
+  const SettingsLayout({super.key, required this.child,required this.title});
+
+  @override
+  State<SettingsLayout> createState() => _SettingsLayoutState();
+}
+
+class _SettingsLayoutState extends State<SettingsLayout> {
+
+  @override
+  Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title), leading: BackButton(onPressed: () => context.pop()), centerTitle: true),
+      body: widget.child,
+    );
+  }
+}
