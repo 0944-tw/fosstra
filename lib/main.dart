ea3eafdac10bdd473c2b01e57cf5344154a04602
zeroninefourfour <zwec181818@proton.me>
2025-09-13 23:10:29 +0800

    Route and Material You Support [FEAT/Main]

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:loader_overlay/loader_overlay.dart';
-import 'package:tra/HomePage.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:tra/l10n/app_localizations.dart';
+import 'package:tra/route.dart';
+import 'package:tra/services/PreferenceService.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'package:tra/views/HomePage/home_page.dart';
+
+// 2. Make main async to await SharedPreferences
+Future<void> main() async {
+  // Required before runApp if you are doing async work in main
+  WidgetsFlutterBinding.ensureInitialized();
+  await PreferenceService().init();
+
+  final bool useMaterialYou = PreferenceService().instance.getBool('enableMaterialYou') ?? true;
+  String? localeString = PreferenceService().instance.getString("locale");
+  Locale? locale;
+  if (localeString != null) {
+    locale =  Locale.fromSubtags(
+        languageCode: localeString
+
+    );
+  }
+  runApp(MainApp(useMaterialYou: useMaterialYou,locale: locale));
 }
 
+
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final bool useMaterialYou;
+  final Locale? locale;
+  const MainApp({super.key, required this.useMaterialYou,this.locale});
 
   @override
   Widget build(BuildContext context) {
+
     return DynamicColorBuilder(
       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-        // Define your fallback color schemes
         ColorScheme lightColorScheme = ColorScheme.fromSeed(
-              seedColor: Color.fromARGB(255, 37, 111, 160),
-             // seedColor: Colors.deepPurple,
-           brightness: Brightness.light,
+          seedColor: Color.fromARGB(255, 37, 111, 160),
+          brightness: Brightness.light,
         );
         ColorScheme darkColorScheme = ColorScheme.fromSeed(
-         seedColor: Color.fromARGB(255, 37, 111, 160),
+          seedColor: Color.fromARGB(255, 37, 111, 160),
           brightness: Brightness.dark,
         );
 
-        /*
-         // If dynamic colors are available, use them
-        if (lightDynamic != null && darkDynamic != null) {
+        if (useMaterialYou && lightDynamic != null && darkDynamic != null) {
           lightColorScheme = lightDynamic;
           darkColorScheme = darkDynamic;
         }
-        */
 
-        return MaterialApp(
-          title: 'btrTRA',
+        return MaterialApp.router(
+          title: 'FossTRA',
           localizationsDelegates: [
+            AppLocalizations.delegate,
             GlobalMaterialLocalizations.delegate,
             GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
           ],
+
           supportedLocales: [
-            Locale('en'), 
-            Locale('tw'),
+            const Locale('en'),
+             Locale.fromSubtags(languageCode: 'zh'),
           ],
+          locale: locale,
           theme: ThemeData(
             colorScheme: lightColorScheme,
-            useMaterial3: true, // Crucial for Material You
+            useMaterial3: true,
           ),
           darkTheme: ThemeData(
             colorScheme: darkColorScheme,
             useMaterial3: true,
           ),
-          themeMode: ThemeMode.light, // Respects system light/dark mode
-          home: HomePage(),
+          themeMode: ThemeMode.system,
+          routerConfig: AppRouter().router,
         );
       },
     );
