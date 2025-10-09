9ff6b591f55c23009a792b0960fa1444333f7191
zeroninefourfour <zwec181818@proton.me>
2025-10-09 10:47:28 +0800

    feat: Shows translation for locations

--- lib/views/LocationSelect/location_select.dart
+++ lib/views/LocationSelect/location_select.dart
@@ -17,48 +17,48 @@ class LocationSelectView extends StatelessWidget {
 
     return ViewModelBuilder<LocationSelectViewModel>.reactive(
       viewModelBuilder: () => LocationSelectViewModel(),
-      onViewModelReady: (model) => model.init(),
+      onViewModelReady: (model) => model.init(context),
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(title: Text(localizations.select)),
         backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
         body: model.isBusy
             ? const Center(child: CircularProgressIndicator())
             : Row(
-          children: [
-            Expanded(
-              flex: 5,
-              child: ListView.builder(
-                itemCount: model.cities.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(model.cities[index].key),
-                    onTap: () {
-                      model.selectCity(index);
-                    },
-                  );
-                },
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: ListView.builder(
+                      itemCount: model.cities.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(model.getLocalizedCityName(model.cities[index].key, loc)),
+                          onTap: () {
+                            model.selectCity(index);
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  //
+                  const VerticalDivider(width: 1.0),
+                  //
+                  Expanded(
+                    flex: 5,
+                    child: ListView.builder(
+                      itemCount: model.selectedStations.length,
+                      itemBuilder: (context, index) {
+                        final station = model.selectedStations[index];
+                        return ListTile(
+                          title: Text(model.getLocalizedStationName(station, loc)),
+                          onTap: () {
+                            model.stationSelected(context, station);
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ),
-            //
-            const VerticalDivider(width: 1.0),
-            //
-            Expanded(
-              flex: 5,
-              child: ListView.builder(
-                itemCount: model.selectedStations.length,
-                itemBuilder: (context, index) {
-                  final station = model.selectedStations[index];
-                  return ListTile(
-                    title: Text(model.getLocalizedStationName(station, loc)),
-                    onTap: () {
-                      model.stationSelected(context,station);
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
--- lib/views/LocationSelect/location_select_viewmodel.dart
+++ lib/views/LocationSelect/location_select_viewmodel.dart
@@ -4,9 +4,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:http/http.dart' as http;
+import 'package:tra/l10n/app_localizations.dart';
 
 class LocationSelectViewModel extends BaseViewModel {
-
   Map<String, List> _stations = {};
   List<MapEntry<String, dynamic>> _stationsList = [];
   List<dynamic> _selectedStations = [];
@@ -14,23 +14,23 @@ class LocationSelectViewModel extends BaseViewModel {
   List<MapEntry<String, dynamic>> get cities => _stationsList;
   List<dynamic> get selectedStations => _selectedStations;
 
-  Future<void> init() async {
+  late BuildContext context;
+  late AppLocalizations localizations;
+  Future<void> init(BuildContext context) async {
     setBusy(true);
+    context = context;
+    localizations = AppLocalizations.of(context)!;
     await _fetchFormattedStations();
     setBusy(false);
   }
 
-  // 3. 業務邏輯 (Business Logic)
   Future<void> _fetchFormattedStations() async {
     try {
       final response = await http.get(
         Uri.parse(
           'https://raw.githubusercontent.com/0944-tw/TaiwanRailwayData/refs/heads/main/TestingData/TRA_Stations.json',
         ),
-        headers: {
-          "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0",
-        },
+        headers: {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0"},
       );
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
@@ -44,19 +44,17 @@ class LocationSelectViewModel extends BaseViewModel {
         }
         _stations = tempStations;
         _stationsList = _stations.entries.toList();
-
       } else {
-      /*
+        /*
         await _dialogService.showDialog(
           title: '請求錯誤',
           description: '狀態碼: ${response.statusCode}\n內容: ${response.body}',
         );
 
        */
-
       }
     } catch (err) {
-       /*
+      /*
        await _dialogService.showDialog(
         title: '發生預期外的錯誤',
         description: err.toString(),
@@ -64,7 +62,7 @@ class LocationSelectViewModel extends BaseViewModel {
 
         */
     }
-     notifyListeners();
+    notifyListeners();
   }
 
   void selectCity(int index) {
@@ -72,8 +70,59 @@ class LocationSelectViewModel extends BaseViewModel {
     notifyListeners();
   }
 
-  void stationSelected(dynamic context,dynamic station) {
+  void stationSelected(dynamic context, dynamic station) {
     Navigator.pop(context, station);
+  }
+
+  String getLocalizedCityName(String station, Locale locale) {
+    switch (station.toLowerCase()) {
+      case "tpe":
+        return localizations.tpe; // Taipei
+      case "kee":
+        return localizations.kee; // Keelung
+      case "nwt":
+        return localizations.nwt; // New Taipei
+      case "tao":
+        return localizations.tao; // Taoyuan
+      case "hsq":
+        return localizations.hsq; // Hsinchu County
+      case "hsz":
+        return localizations.hsz; // Hsinchu City
+      case "mia":
+        return localizations.mia; // Miaoli
+      case "txg":
+        return localizations.txg; // Taichung
+      case "cha":
+        return localizations.cha; // Changhua
+      case "nan":
+        return localizations.nan; // Nantou
+      case "yun":
+        return localizations.yun; // Yunlin
+      case "cyq":
+        return localizations.cyq; // Chiayi County
+      case "cyi":
+        return localizations.cyi; // Chiayi City
+      case "tnn":
+        return localizations.tnn; // Tainan
+      case "khh":
+        return localizations.khh; // Kaohsiung
+      case "pif":
+        return localizations.pif; // Pingtung
+      case "ttt":
+        return localizations.ttt; // Taitung
+      case "hua":
+        return localizations.hua; // Hualien
+      case "ila":
+        return localizations.ila; // Yilan
+      case "pen":
+        return localizations.pen; // Penghu
+      case "kin":
+        return localizations.kin; // Kinmen
+      case "ltt":
+        return localizations.ltt; // Lienchiang
+      default:
+        return station; // fallback
+    }
   }
 
   String getLocalizedStationName(dynamic station, Locale locale) {
